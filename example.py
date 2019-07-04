98eb055494fb5c0cf3e1ea3c65e0dd75fb8bf3be
Li SH <li012589li@outlook.com>
2019-07-04 23:34:19 +0800

    minor change

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from torch.autograd import grad
 import numpy as np
 from scipy import integrate
 
-from utils import stormerVerlet
+from src import stormerVerlet
 
 def diff(f,t,x):
     res= f(t,x)
@@ -21,11 +21,9 @@ def harmonic(alpha,t,eta):
     num = eta.shape[1]//2
     return 0.5*(eta[:,:num]**2).sum(-1)+alpha*0.5*(eta[:,num:]**2).sum(-1)
 
-
-
 H = lambda t,eta: harmonic(1,1,eta)
 
-def hami(q,p):
+def HH(q,p):
     ETA = torch.cat((q,p),dim=-1)
     return harmonic(1,1,ETA)
 
@@ -41,7 +39,7 @@ eta = torch.cat((q,p),dim=-1)
 
 sciInt = integrate.solve_ivp(F,[0,h*step],y0 = eta.reshape(-1).numpy(),method='RK45',rtol=1e-6)
 
-myQ,myP = stormerVerlet(q,p,hami,h,step)
+myQ,myP = stormerVerlet(q,p,HH,h,step)
 myQ = myQ.reshape(-1).numpy()
 myP = myP.reshape(-1).numpy()
 
